4a9de5e9b1fd6a2858f143e458f2042eb1b3a3bb
The-End-Hero <527409987@qq.com>
2019-05-24 18:09:35 +0800

    feat: some code change

---
+++ stories/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { name, repository, author } from "../package.json";
+import { FaGithub } from "react-icons/fa";
+import ReactMarkDown from "react-markdown";
+
+const LOGO = `
+                                      
+`;
+
+storiesOf("综述", module)
+  .add("介绍", () => (
+    <article style={{ padding: 20 }} className="index-page">
+      <h1 style={{ fontSize: 40, padding: 0, margin: 0 }}>
+        {name}
+      </h1>
+    </article>
+  ))
