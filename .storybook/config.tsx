034b8e9a0e814178f2231efa913ef1835a41044e
The-End-Hero <527409987@qq.com>
2019-05-24 09:53:06 +0800

    docs: some code change

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -1,47 +1,16 @@
 import React from "react";
 import { configure, addDecorator, addParameters } from "@storybook/react";
 import { themes } from "@storybook/theming";
-import { name, repository } from "../package.json";
+import { name, repository, version} from "../package.json";
 import { withInfo } from "@storybook/addon-info";
 import { withNotes } from "@storybook/addon-notes";
 import { configureActions } from "@storybook/addon-actions";
 import { withOptions } from "@storybook/addon-options";
-import { version } from "../package.json";
 import "@storybook/addon-console";
 // import "../components/styles/index.less";
 import "../stories/styles/code.less";
 
 function loadStories() {
-  // 介绍
-  // require('../stories/index');
-  // // 普通
-  // require('../stories/general');
-  // // 视听娱乐
-  // require('../stories/player');
-  // // 导航
-  // require('../stories/navigation')
-  // // 数据录入
-  // require('../stories/dataEntry');
-  // // 数据展示
-  // require('../stories/dataDisplay');
-  // // 布局
-  // require('../stories/grid');
-  // // 操作反馈
-  // require('../stories/feedback');
-  // // 其他
-  // require('../stories/other');
-
-
-  // SketchPicker
-  // require("../stories/sketchpicker");
-  // // mapcards
-  // require("../stories/mapcards");
-  // // amap
-  // require("../stories/amap");
-  // // maptalks
-  // require('../stories/maptalks')
-  // // 全量地图展示
-  // require('../stories/allmapshow')
   // // 地图工具栏
   require("../stories/maptool");
 }
